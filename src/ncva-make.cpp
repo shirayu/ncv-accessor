1c56e35b051191ad5f947ae70e73f6b8017cef2e
yuta-h <yuta-h@is.naist.jp>
2013-05-06 12:49:14 +0900

    Add mode2

--- src/ncva-make.cpp
+++ src/ncva-make.cpp
@@ -12,18 +12,14 @@
 
 namespace ncva{
 
-    void make(const char* output_name, std::istream *is, const uint32_t line_num, const bool mode_VCN){
+    void make(const char* output_name, std::istream *is, const uint32_t line_num, const int mode){
         MAP_FREQUENCY mymap;
         FREQUENCY_LONG sum = 0;
         uint32_t step = 0;
+        FREQUENCY freq = 0;
 
         std::string buf;
         while(std::getline(*is, buf)) {
-            const std::vector<std::string> items = split(buf, '\t');
-            if (items.size() != 4){
-                std::cerr << "Error at " << step << std::endl;
-                continue;
-            };
             if(step % 100000 == 0){
                 fprintf(stderr, "\rStatus : %u", step);
                 if (line_num != 0){
@@ -32,21 +28,42 @@ namespace ncva{
                 };
             };
 
-            const std::string& noun = items[0];
-            const std::string& particle = items[1];
-            const std::string& verb = items[2];
-            const std::string& freq_word = items[3];
-            const FREQUENCY freq = atoi(freq_word.c_str()); //FIXME change to strtol
-        
-            if (mode_VCN){
-                mymap[verb] += freq;
-                mymap[verb + "\t" + particle ] += freq;
-                mymap[verb + "\t" + particle + "\t" + noun] += freq;
+            if (mode == 0 or mode == 1){
+                const std::vector<std::string> items = split(buf, '\t');
+                if (items.size() != 4){
+                    std::cerr << "Error at " << step << std::endl;
+                    continue;
+                };
+
+                const std::string& noun = items[0];
+                const std::string& particle = items[1];
+                const std::string& verb = items[2];
+                const std::string& freq_word = items[3];
+                freq = atoi(freq_word.c_str()); //FIXME change to strtol
+            
+                if (mode == 1){
+                    mymap[verb] += freq;
+                    mymap[verb + "\t" + particle ] += freq;
+                    mymap[verb + "\t" + particle + "\t" + noun] += freq;
+                }
+                else{
+                    mymap[noun] += freq;
+                    mymap[noun + "\t" + particle ] += freq;
+                    mymap[noun + "\t" + particle + "\t" + verb] += freq;
+                };
+            }
+            else if(mode==2){
+                for(size_t i= buf.size()-1; i>=0; --i){
+                    if(buf[i] == '\t'){
+                        freq = atoi(buf.substr(i+1).c_str());
+                        mymap[buf.substr(0, i)] = freq;
+    std::cerr << freq  << "|\t|" << buf.substr(0, i) << "|" << std::endl;
+                        break;
+                    };
+                };
+
             }
             else{
-                mymap[noun] += freq;
-                mymap[noun + "\t" + particle ] += freq;
-                mymap[noun + "\t" + particle + "\t" + verb] += freq;
             };
             sum += freq;
 
@@ -73,7 +90,7 @@ int main(int argc, char **argv) {
     arg_parser.add<std::string>("output", 'o', "The filename to output", true, "");
     arg_parser.add<std::string>("input", 'i', "The filename to input.\n\t'-' means standard input is designated.", true, "");
     arg_parser.add<uint32_t>("lines", 'l', "The number of line", false, 0);
-    arg_parser.add("mode", 'm', "Switch the mode");
+    arg_parser.add<int>("mode", 'm', "Switch the mode", true, 0);
 
         
     arg_parser.parse_check(argc, argv);
@@ -88,7 +105,7 @@ int main(int argc, char **argv) {
         is = new std::ifstream (input.c_str());
     };
     const uint32_t line_num = arg_parser.get<uint32_t>("lines");
-    const bool mode = arg_parser.exist("mode");
+    const int mode = arg_parser.get<int>("mode");
     std::cerr << "Mode is " << mode << std::endl;
     ncva::make(output_name, is, line_num, mode);
 
