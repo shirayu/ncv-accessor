360e42100f55ada6414e41586b7645a35ea1361f
yuta-h <yuta-h@is.naist.jp>
2013-05-07 15:39:25 +0900

    update

--- src/array.cpp
+++ src/array.cpp
@@ -12,7 +12,7 @@
 namespace ncva{
 
 
-    NaiveArray::NaiveArray(const char *filename, const bool write) : ifs(NULL), ofs(NULL), filesize(0){
+    NaiveArray::NaiveArray(const char *filename, const bool write) : mmap_(NULL), ofs(NULL), filesize(0){
         if (filename == NULL){
         }
         else{
@@ -20,15 +20,24 @@ namespace ncva{
         		this->ofs = new std::ofstream(filename, std::ios::binary);
             }
             else{
-        		this->ifs = new std::ifstream(filename, std::ios::binary);
-                this->filesize = (size_t) this->ifs->seekg(0, std::ios::end).tellg();
-                this->ifs->seekg(0, std::ios::beg);  
+                this->open(filename);
             };
         };
     };
 
+    bool NaiveArray::open(const char *filename){
+        this->mmap_ = new Mmap<FREQUENCY>;
+        CHECK_FALSE(mmap_->open(filename)) <<  mmap_->what();
+        this->filesize = this->mmap_->size();
+        //                this->mmap->seekg(0, std::ios::beg);  
+        return true;
+    };
+
     NaiveArray::~NaiveArray(){
-        delete this->ifs;
+        if (this->mmap_ != NULL){
+            this->mmap_->close();
+            delete this->mmap_;
+        };
         delete this->ofs;
     };
 
@@ -39,12 +48,14 @@ namespace ncva{
     };
 
     FREQUENCY NaiveArray::get(const ID_t id){
-        const size_t position = sizeof(FREQUENCY) * id;
+//        const size_t position = sizeof(FREQUENCY) * id;
+        const size_t position = id;
         if (position < this->filesize){
-            ifs->seekg(position, std::ios_base::beg);
-            FREQUENCY freq;
-            ifs->read((char *) &freq, sizeof(FREQUENCY));
-            return freq;
+            return (*(this->mmap_))[position];
+//            mmap_->seekg(position, std::ios_base::beg);
+//            FREQUENCY freq;
+//            mmap_->read((char *) &freq, sizeof(FREQUENCY));
+//            return freq;
         };
         return 0;
     };
